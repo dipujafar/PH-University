0503ab97e7eaae5aeda9211a8ec01f98657f8cb7
dipujafar <dipujafarmr@gmail.com>
2024-06-22 23:22:17 +0600

    implement organize error validation respones

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,6 +4,7 @@ import path from 'path';
 dotenv.config();
 
 export default {
+  NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_password: process.env.DEFAULT_PASS,
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,41 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
+import handleZodError from '../error/handleZodError';
+import handleValidationError from '../error/handleValidationError';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err?.message || 'Something Went wrong';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err?.message || 'Something Went wrong';
+
+  let errorSource: TErrorSources = [
+    {
+      path: '',
+      message: 'Something Went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSources;
+  }
+  // ultimate return
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
+    stack: config?.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
